eb83f6260a94255d4890439cfe68781affc32d24
JonMa327-art <71156814+JonMa327-art@users.noreply.github.com>
2022-03-30 13:51:49 -0400

    Setting project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+//Third party imports
+import './vendors/bootstrap/css/bootstrap.min.css'
+import './vendors/bootstrap/bootstrap.min.css';
+import './vendors/fontawesome/css/all.min.css';
+
+//CSS imports
+import './App.css';
+
+//App Function
+function App() {
+  return (
+    <div className='container'>
+      <h1>Hello World!!</h1>
+    </div>
+  );
+}
+
+export default App;
